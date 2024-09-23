a176e89ced939af786894fd3853ca4706ec99c46
siddharthdhodi05 <siddharth.dhodi.work@gmail.com>
2024-09-23 21:51:21 +0530

    Setup-complete

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+
+function App() {
+ 
+
+  return (
+    <>
+     <h1 className=" p-10 bg-green-300 shadow-2xl text-red-600 text-9xl align-middle m-14
+     "> initial set-up
+     </h1>
+    </>
+  )
+}
+
+export default App
